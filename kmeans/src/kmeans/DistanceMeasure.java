39da69602c0ad0be11d604fccdcd986dd8ad38b7
Aniket <aniket@localhost.localdomain>
2016-04-03 21:12:48 -0400

    Reorganize DistanceMeasure

--- kmeans/src/kmeans/DistanceMeasure.java
+++ kmeans/src/kmeans/DistanceMeasure.java
@@ -3,14 +3,14 @@ package org.myorg;
 import java.util.HashMap;
 
 public class DistanceMeasure {
-	public double CosineMeasure(Vector v1, Vector v2)
+	public double CosineMeasure(Vector A, Vector B)
 	{
 		double simMeasure=0;
-		HashMap<Integer, Integer> vec1=new HashMap<Integer, Integer>();
-		HashMap<Integer, Integer> vec2=new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> vec1=A.getFeatures();
+		HashMap<Integer, Integer> vec2=B.getFeatures();
 		double numerator=0;
-		double magA=1;
-		double magB=1;
+		double magnitudeA=1;
+		double magnitudeB=1;
 		for(int t : vec1.keySet())
 		{
 			if(vec2.containsKey(t))
@@ -18,28 +18,28 @@ public class DistanceMeasure {
 				numerator += vec1.get(t) * vec2.get(t);
 				
 			}
-			magA += t^2;
+			magnitudeA += t^2;
 			
 		}
-		magA = Math.sqrt(magA);
+		magnitudeA = Math.sqrt(magnitudeA);
 		
 		for(int t : vec2.keySet())
 		{
-			magB += t^2;
+			magnitudeB += t^2;
 		}
-		magB = Math.sqrt(magB);
+		magnitudeB = Math.sqrt(magnitudeB);
 		
-		simMeasure = numerator / ( magA * magB );
+		simMeasure = numerator / ( magnitudeA * magnitudeB );
 		return simMeasure;
 	}
-	public double JaccardMeasure(Vector v1, Vector v2)
+	public double JaccardMeasure(Vector A, Vector B)
 	{
 		double simMeasure=0;
-		HashMap<Integer, Integer> vec1=new HashMap<Integer, Integer>();
-		HashMap<Integer, Integer> vec2=new HashMap<Integer, Integer>();
-		double numerator=0;
-		double magA=1;
-		double magB=1;
+		HashMap<Integer, Integer> vec1=A.getFeatures();
+		HashMap<Integer, Integer> vec2=B.getFeatures();
+		double commonData=0;
+		double magnitudeA=1;
+		double magnitudeB=1;
 		for(int t : vec1.keySet())
 		{
 			
@@ -47,20 +47,20 @@ public class DistanceMeasure {
 			{
 				
 				if(vec1.get(t)<vec2.get(t))
-					numerator += vec1.get(t);
+					commonData += vec1.get(t);
 				else
-					numerator += vec2.get(t);
+					commonData += vec2.get(t);
 				
 				
 			}
-			magA += vec1.get(t);
+			magnitudeA += vec1.get(t);
 			
 		}
 		for(int t: vec2.keySet())
 		{
-			magB += vec2.get(t);
+			magnitudeB += vec2.get(t);
 		}
-		simMeasure = (numerator) / (magA + magB - numerator);
+		simMeasure = (commonData) / (magnitudeA + magnitudeB - commonData);
 		
 		
 		return simMeasure;
