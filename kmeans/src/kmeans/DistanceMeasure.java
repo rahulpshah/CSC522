eb8f4003069720d704147b5d13e759232424c7c4
Aniket <aniket@localhost.localdomain>
2016-04-03 21:06:44 -0400

    Jaccard Measure

--- kmeans/src/kmeans/DistanceMeasure.java
+++ kmeans/src/kmeans/DistanceMeasure.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class DistanceMeasure {
 	public double CosineMeasure(Vector v1, Vector v2)
 	{
-		double disMeasure=0;
+		double simMeasure=0;
 		HashMap<Integer, Integer> vec1=new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> vec2=new HashMap<Integer, Integer>();
 		double numerator=0;
@@ -29,7 +29,41 @@ public class DistanceMeasure {
 		}
 		magB = Math.sqrt(magB);
 		
-		disMeasure = numerator /( magA * magB );
-		return disMeasure;
+		simMeasure = numerator / ( magA * magB );
+		return simMeasure;
+	}
+	public double JaccardMeasure(Vector v1, Vector v2)
+	{
+		double simMeasure=0;
+		HashMap<Integer, Integer> vec1=new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> vec2=new HashMap<Integer, Integer>();
+		double numerator=0;
+		double magA=1;
+		double magB=1;
+		for(int t : vec1.keySet())
+		{
+			
+			if(vec2.containsKey(t))
+			{
+				
+				if(vec1.get(t)<vec2.get(t))
+					numerator += vec1.get(t);
+				else
+					numerator += vec2.get(t);
+				
+				
+			}
+			magA += vec1.get(t);
+			
+		}
+		for(int t: vec2.keySet())
+		{
+			magB += vec2.get(t);
+		}
+		simMeasure = (numerator) / (magA + magB - numerator);
+		
+		
+		return simMeasure;
+		
 	}
 }
